29f2955d42f2d8ff2d791cffbcd9046a541e37e6
Phillip Conrad <pconrad@cs.ucsb.edu>
2025-04-02 18:02:53 -0700

    pc - update to S25

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -17,7 +17,7 @@ public class Developer {
 
     public static String getName() {
         // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-f24-teams>
+        // You may use just the name that is used on <https://bit.ly/cs156-s25-teams>
         // i.e. your first name, or your first and initial of last name
 
         return "Chris G.";
@@ -40,7 +40,7 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("f24-xx");
+        Team team = new Team("s25-xx");
         team.addMember("Alice");
         team.addMember("Bob");
         team.addMember("Chris G.");
--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -23,7 +23,7 @@ public class DeveloperTest {
     @Test
     public void getName_returns_correct_name() {
         // TODO: Replace Chris G. with your name as shown on
-        // <https://bit.ly/cs156-f24-teams>
+        // <https://bit.ly/cs156-s25-teams>
         assertEquals("Chris G.", Developer.getName());
     }
 
