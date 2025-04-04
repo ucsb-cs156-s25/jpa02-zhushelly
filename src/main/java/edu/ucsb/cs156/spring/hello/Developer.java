db4d515d8354d5ae7fe6ad6dd321a43009f866df
Shelly Zhu <zhixiuzhu@umail.ucsb.edu>
2025-04-03 21:08:34 -0700

    xy - updated with actual developer name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,11 +16,7 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-s25-teams>
-        // i.e. your first name, or your first and initial of last name
-
-        return "Chris G.";
+        return "Shelly Z";
     }
 
     /**
@@ -30,7 +26,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "zhushelly";
     }
 
     /**
@@ -40,13 +36,13 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("s25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s25-07");
+        team.addMember("Hannah");
+        team.addMember("Annika");
+        team.addMember("Forrest");
+        team.addMember("Hannya");
+        team.addMember("Yuchen");
+        team.addMember("Shelly");
         return team;
     }
 }
